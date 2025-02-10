042b5238db0bfa15300b43e18afaf29b7f29b55b
boracingoz26 <boracingoz@gmail.com>
2025-02-10 22:18:12 +0300

    MainMenuPanel  added.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,26 +4,34 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-
     [SerializeField] private GameManager _gameManager;
-    [SerializeField] private AudioSource _ballAudio; 
+    [SerializeField] private AudioSource _ballAudio;
+    private Rigidbody _ballRb; // Rigidbody referansı
 
-   void OnTriggerEnter(Collider other)
-   {
+    private void Start()
+    {
+        _ballRb = GetComponent<Rigidbody>(); // Rigidbody bileşenini al
+    }
 
-        if (other.CompareTag("Basket"))
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Basket")) 
         {
+            if (_ballRb.velocity.y < 0) // Eğer top aşağı doğru hareket ediyorsa sayı sayma
+            {
+                return;
+            }
+
             _gameManager.Basket(transform.position);
         }
-        else if(other.CompareTag("GameOver"))
+        else if (other.CompareTag("GameOver")) // Buradaki hatalı else yapısını düzelttim
         {
-           _gameManager.GameOver();
+            _gameManager.GameOver();
         }
-   }
+    }
 
-
-   void OnCollisionEnter(Collision other)
-   {
+    void OnCollisionEnter(Collision other)
+    {
         _ballAudio.Play();
-   }
+    }
 }
--- Assets/Scripts/Managers/MaınMenuManager.cs
+++ Assets/Scripts/Managers/MaınMenuManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using TMPro;
 
 public class MaınMenuManager : MonoBehaviour
 {
