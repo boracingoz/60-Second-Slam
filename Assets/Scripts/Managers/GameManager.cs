03ed0f7b24e6c7a80c3dc8c1e6614caafda9c2ac
boracingoz26 <boracingoz@gmail.com>
2025-02-21 10:09:39 +0300

    Bugs fixed

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -57,12 +57,12 @@ public class GameManager : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             if (_platform.transform.position.x > -7.3)
-                _platform.transform.position = Vector3.Lerp(_platform.transform.position, new Vector3(_platform.transform.position.x - .3f, _platform.transform.position.y, _platform.transform.position.z), 0.50f);
+                _platform.transform.position = Vector3.Lerp(_platform.transform.position, new Vector3(_platform.transform.position.x - .5f, _platform.transform.position.y, _platform.transform.position.z), 0.50f);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             if (_platform.transform.position.x < 7.3)
-                _platform.transform.position = Vector3.Lerp(_platform.transform.position, new Vector3(_platform.transform.position.x + .3f, _platform.transform.position.y, _platform.transform.position.z), 0.50f);
+                _platform.transform.position = Vector3.Lerp(_platform.transform.position, new Vector3(_platform.transform.position.x + .5f, _platform.transform.position.y, _platform.transform.position.z), 0.50f);
         }
 
         #if UNITY_EDITOR
